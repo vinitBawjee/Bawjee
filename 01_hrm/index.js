a0a5599b6f25307152176d8d1fbf9e00437b0af7
Vinit Bawjee <bawjeeai@gmail.com>
2025-07-17 23:23:01 +0530

    login page

--- 01_hrm/index.js
+++ 01_hrm/index.js
@@ -1,8 +1,10 @@
 let in_out_time = 0;
+let intervalId = null; 
 
 function Check_In_time() {
+    if (intervalId !== null) return;
 
-    setInterval(() => {
+    intervalId = setInterval(() => {
         in_out_time++;
 
         let hrs = parseInt(in_out_time / 3600);
@@ -10,13 +12,25 @@ function Check_In_time() {
         let secs = in_out_time % 60;
 
         let timeStr =
-        (hrs < 10 ? '0' : '') + hrs + ':' +
-        (mins < 10 ? '0' : '') + mins + ':' +
-        (secs < 10 ? '0' : '') + secs;
+            (hrs < 10 ? '0' : '') + hrs + ':' +
+            (mins < 10 ? '0' : '') + mins + ':' +
+            (secs < 10 ? '0' : '') + secs;
 
         document.getElementById('in-out-time').innerHTML = timeStr;
-    }, 1000)
-
+    }, 1000);
 }
 
-// Check_In_time()
+let check_in_btn = document.getElementById('check_in_btn');
+let check_stop_btn = document.getElementById('check_stop_btn');
+
+check_in_btn.addEventListener('click', function () {
+    Check_In_time();
+    console.log(intervalId);
+    
+});
+
+check_stop_btn.addEventListener('click', function () {
+    clearInterval(intervalId);
+    intervalId = null; 
+});
+
