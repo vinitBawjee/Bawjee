390e6d7e1a1029f63083c51d1cdac1166341d1af
Vinit Bawjee <bawjeeai@gmail.com>
2025-07-18 23:27:07 +0530

    login / signup page

---
+++ 01_hrm/login.js
@@ -0,0 +1,7 @@
+function saveData(event) {
+    event.preventDefault(); 
+    let username = document.getElementById("userName").value;
+    let password = document.getElementById("Password").value;
+    console.log("Username:", username);
+    console.log("Password:", password);
+}
